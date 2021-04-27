773f69016c1799e6e83135603bc4329d37350cc0
26seung <yousi7@naver.com>
2021-04-28 03:15:01 +0900

    Basic Practice .js

---
+++ p02/index.js
@@ -0,0 +1,39 @@
+const a = 221;
+let b = a - 5;
+console.log(b);
+
+// 상수 변수 : let 은 변수 변경이 허용, const 는 변수 변경이 불가능 하다
+
+/* 
+String
+const what = "euseung";
+*/
+/*
+Boolean
+const wat = true;
+*/
+/*
+Number
+const wat = 666;
+*/
+/*
+Float
+const wat = 55.1;
+*/
+
+// 배열
+const dayOfWeek = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+console.log(dayOfWeek);
+console.log(dayOfWeek[2]);
+
+// 오브젝트
+const profile = {
+    name:"Euseung",
+    age:31,
+    gender:"Male",
+    isHuman:true,
+    home:"seoul"
+}
+
+console.log(profile.name);
+console.log(profile.home);
