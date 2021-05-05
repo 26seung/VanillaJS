04931cca39bd4be44005eaede198cb418ce5cf4a
26seung <yousi7@naver.com>
2021-05-05 16:27:15 +0900

    saving the User Name

--- p07/gretting.js
+++ p07/gretting.js
@@ -5,24 +5,42 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
-function pantingGreeting(text) {
+// 기억을 못하기 때문에 저장용 함수 추가
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    // console.log(currentValue);
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForname() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
 
-function loadname(){
+function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null) {
-
+        askForname();
     }else {
-        pantingGreeting(currentUser);
+        paintGreeting(currentUser);
     }
 }
 
 
 function init() {
-    loadname();
+    loadName();
 }
 init();
 
@@ -38,6 +56,6 @@ init();
                         localStorage.getItem("euseung") 시 true 라는 value 값을 가져올 수 있음
 
 
-    loadname 함수 통해서 localStorage를 읽어 옴
+    loadname 함수 통해서 localStorage를 읽어 오도록 하자. 값이 있을때 와 없을때를 적용
 
 */ 
